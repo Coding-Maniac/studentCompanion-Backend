6d458217846dc0795ce81f593812e38108e9a7e7
Sanjay Kapilesh <ufoundsanjay@gmail.com>
2021-08-02 21:34:34 +0530

    Localhost in terminal

--- app.js
+++ app.js
@@ -5,33 +5,33 @@ const app = express()
 const attendance = require('./utils/attendance')
 // Define path for Express Config
 const publicDirPath = path.join(__dirname, "public")
-const viewsPath = path.join(__dirname,"templates/views")
-const partialsPath = path.join(__dirname,"templates/partials")
+const viewsPath = path.join(__dirname, "templates/views")
+const partialsPath = path.join(__dirname, "templates/partials")
 // port configuarion
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3030
 
 // Setting up the template engine
-app.set('view engine','hbs')
+app.set('view engine', 'hbs')
 // Setting up the path for template engine
-app.set('views',viewsPath)
+app.set('views', viewsPath)
 
 hbs.registerPartials(partialsPath);
 // To serve static files == Mostly not needed
 app.use(express.static(publicDirPath))
-app.get('',(req,res) => {
+app.get('', (req, res) => {
     res.render('index')
 })
 
-app.get('/attendance',(req,res)=>{
-    if(!req.query.rollNumber || !req.query.password ){
+app.get('/attendance', (req, res) => {
+    if (!req.query.rollNumber || !req.query.password) {
         return res.send({
             error: "Login and Password Must be provided"
         })
     }
-    else{
+    else {
         let rollNumber = req.query.rollNumber
         let password = req.query.password
-        attendance(rollNumber,password).then((val) => {
+        attendance(rollNumber, password).then((val) => {
             return res.send(val)
         }).catch((err) => {
             res.send({
@@ -41,6 +41,6 @@ app.get('/attendance',(req,res)=>{
     }
 })
 
-app.listen(port,() => {
-    console.log(`Server is up at localhost:${port}`)
+app.listen(port, () => {
+    console.log(`Server is up at http://localhost:${port}`)
 })
--- utils/attendance.js
+++ utils/attendance.js
@@ -1,24 +1,24 @@
 const { default: Cheerio } = require('cheerio');
-const {Builder, By, Key} = require('selenium-webdriver');
+const { Builder, By, Key } = require('selenium-webdriver');
 const attendance = async (rollNumber, password) => {
     let driver = await new Builder().forBrowser('chrome').build()
     await driver.get('https://studentportal.hindustanuniv.ac.in');
-    let inp =  driver.findElement(By.name('username_temp'))
+    let inp = driver.findElement(By.name('username_temp'))
     await inp.sendKeys(rollNumber)
-    let pass =  driver.findElement(By.name('password'))
+    let pass = driver.findElement(By.name('password'))
     await pass.sendKeys(password, Key.ENTER)
-    let btn =  driver.findElement(By.css('button[onclick="signIn(\'HITS\')"]'))
+    let btn = driver.findElement(By.css('button[onclick="signIn(\'HITS\')"]'))
     await btn.sendKeys('1', Key.ENTER)
-    let academics =  driver.findElement(By.css('li[class="dropdown"]:nth-of-type(3) a'))
+    let academics = driver.findElement(By.css('li[class="dropdown"]:nth-of-type(3) a'))
     await academics.sendKeys('1', Key.ENTER)
     let tt = driver.findElement(By.css('a[href="/search/subjAttendReport.htm"]'))
-    await tt.sendKeys('1',Key.ENTER)
-    function setDate(){
-        document.getElementsByName("fromDate")[0].value="03/01/2021"
-        document.getElementsByName("toDate")[0].value="15/05/2021"
+    await tt.sendKeys('1', Key.ENTER)
+    function setDate() {
+        document.getElementsByName("fromDate")[0].value = "03/01/2021"
+        document.getElementsByName("toDate")[0].value = "15/05/2021"
     }
     driver.executeScript(setDate)
-    let btn2 =  driver.findElement(By.css('input[type="submit"]'))
+    let btn2 = driver.findElement(By.css('input[type="submit"]'))
     await btn2.sendKeys('1', Key.ENTER)
     let html = driver.executeScript("return document.documentElement.outerHTML").then(
         res => {
@@ -27,11 +27,11 @@ const attendance = async (rollNumber, password) => {
     )
     return html
 }
-function attendanceFinal(res){
+function attendanceFinal(res) {
     let $ = Cheerio.load(res);
     let numberOfSubjects = $('div[id="tableDetailDiv"] tbody tr').toArray().length
     let subjects = {}
-    for(let i=1;i<=numberOfSubjects;i++){
+    for (let i = 1; i <= numberOfSubjects; i++) {
         let subjName = $(`div[id="tableDetailDiv"] tbody tr:nth-of-type(${i}) td:nth-of-type(1)`).text()
         let totalHours = $(`div[id="tableDetailDiv"] tbody tr:nth-of-type(${i}) td:nth-of-type(2)`).text()
         let totalHoursPresent = $(`div[id="tableDetailDiv"] tbody tr:nth-of-type(${i}) td:nth-of-type(3)`).text()
@@ -40,17 +40,17 @@ function attendanceFinal(res){
             totalHoursPresent
         }
     }
-    let updatedSubjects = attendanceObjectMaker(subjects) ;
+    let updatedSubjects = attendanceObjectMaker(subjects);
     return updatedSubjects
 }
-function attendanceObjectMaker(subjects){
+function attendanceObjectMaker(subjects) {
     for (let sub in subjects) {
         let val = subjects[sub]
-        let ans = attendanceFinder(val.totalHours,val.totalHoursPresent)
-        if(ans === false){
+        let ans = attendanceFinder(val.totalHours, val.totalHoursPresent)
+        if (ans === false) {
             subjects[sub].above = true
         }
-        else{
+        else {
             subjects[sub].above = false
             subjects[sub].numberOfClassesToAttend = ans
         }
@@ -59,7 +59,7 @@ function attendanceObjectMaker(subjects){
 }
 function attendanceFinder(totalClasses, AttendedClasses) {
     let attendance = AttendedClasses / totalClasses
-    if(attendance >= 0.75){
+    if (attendance >= 0.75) {
         return false
     }
     let numberOfClassesToAttend = (totalClasses * 0.75) - AttendedClasses;
