d06621db3d7862cdd7f9f6912aad69221f1916c7
Sanjay Kapilesh <ufoundsanjay@gmail.com>
2021-08-03 07:11:25 +0530

    Completed Grade data extraction

--- utils/grades.js
+++ utils/grades.js
@@ -12,42 +12,57 @@ const grades = async (rollNumber, password) => {
     let tt = driver.findElement(By.css('a[href="/search/gradeSheet.htm"]'))
     await tt.sendKeys('1', Key.ENTER)
 
-    // Select Button
-    // let semBtn = driver.findElement(By.css('select[name="semester"]'))
-    // await semBtn.sendKeys('1', Key.ENTER)
-
-    // // Selecting Option1
-    // let selectOption = driver.findElement(By.css('option[value="1"]'))
-    // selectOption.sendKeys('1', Key.ENTER)
-
-    const selectSemester = async () => {
-        document.getElementById('semester').selectedIndex = 1
-
-    }
-    await driver.executeScript(selectSemester)
-    await driver.findElement(By.css('input[type="submit"]')).sendKeys('1', Key.ENTER)
-    await driver.executeScript("return document.documentElement.innerHTML").then(
-        html => {
-            gradeCalculator(html)
+    for (let i = 1; i < 9; i++) {
+        const selectSemester = async (index) => {
+            document.getElementById('semester').selectedIndex = index
         }
-    )
-
+        await driver.executeScript(selectSemester, i)
+        await driver.findElement(By.css('input[type="submit"]')).sendKeys('1', Key.ENTER)
+        await driver.executeScript("return document.documentElement.innerHTML").then(
+            html => {
+                gradeCalculator(html, i)
+            }
+        )
+    }
+    console.log(gradesObject)
 }
 
-const gradeCalculator = (grades) => {
+const gradesObject = {}
+
+const gradeCalculator = (grades, i) => {
 
     // Load the document in Cheerio
     let $ = Cheerio.load(grades);
-    // console.log($)
     let numberOfSubjects = $('tbody tr').toArray().length
-
+    gradesObject[`semester${i}`] = {}
+    let currentSem = gradesObject[`semester${i}`]
+    currentSem.subjects = {}
     // To avoid the additional CGPA, GPA and TotalCredits 
     numberOfSubjects = numberOfSubjects - 3
-    for (let i = 1; i < numberOfSubjects; i++) {
-        let text = $(`tbody tr:nth-of-type(${i}) td:nth-of-type(2)`).text()
-        console.log(text)
+    for (let j = 1; j < numberOfSubjects; j++) {
+        let subjectCode = $(`tbody tr:nth-of-type(${j}) td:nth-of-type(2)`).text()
+        let marks = $(`tbody tr:nth-of-type(${j}) td:nth-of-type(5)`).text()
+        currentSem.subjects[`${subjectCode}`] = marks
     }
 
+    // GPA
+    let gpaText = $(`tbody tr:nth-of-type(${numberOfSubjects}) th`).text()
+    if (gpaText === "GPA") {
+        let gpa = $(`tbody tr:nth-of-type(${numberOfSubjects}) td`).text()
+        currentSem.gpa = gpa
+    } else {
+        currentSem.gp = null
+    }
+
+    // CGPA
+    let cgpaText = $(`tbody tr:nth-of-type(${numberOfSubjects + 1}) th`).text()
+    if (cgpaText === "CGPA") {
+        let cgpa = $(`tbody tr:nth-of-type(${numberOfSubjects + 1}) td`).text()
+        currentSem.cgpa = cgpa.split('/')[0]
+    } else {
+        currentSem.cgpa = null
+    }
 }
+
 
 grades(18113075, 123456)
