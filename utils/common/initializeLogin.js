dbc1e376badbd5d32affaf2739c0dad4cb0baf6d
Sanjay Kapilesh <ufoundsanjay@gmail.com>
2021-08-03 11:44:43 +0530

    Chrome Driver changes for serve

--- utils/common/initializeLogin.js
+++ utils/common/initializeLogin.js
@@ -1,17 +1,18 @@
 // Common Module to initialize All logins
 const { Builder, By, Key } = require('selenium-webdriver');
 const chrome = require("selenium-webdriver/chrome");
-const chromeOptions = new chrome.Options();
-chromeOptions.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-chromeOptions.addArguments("start-maximized"); // open Browser in maximized mode
-chromeOptions.addArguments("disable-infobars"); // disabling infobars
-chromeOptions.addArguments("--disable-extensions"); // disabling extensions
-chromeOptions.addArguments("--no-sandbox");
-chromeOptions.headless = true
+const options = new chrome.Options();
+options.addArguments("start-maximized"); // open Browser in maximized mode
+options.addArguments("disable-infobars"); // disabling infobars
+options.addArguments("--disable-extensions"); // disabling extensions
+options.addArguments("--disable-gpu"); // applicable to windows os only
+options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+options.addArguments("--no-sandbox");
+options.headless = true
 const wrapper = (rollNumber, password) => initializeLogin(rollNumber, password)
 const initializeLogin = async (rollNumber, password) => {
     // Initializing the driver
-    let driver = await new Builder().forBrowser('chrome').setChromeOptions(chromeOptions).build()
+    let driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build()
     await driver.get('https://studentportal.hindustanuniv.ac.in');
 
     // Entering Username
