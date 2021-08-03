7845349c23f7753c52ec85f2813a58fe4bbf6a63
Sanjay Kapilesh <ufoundsanjay@gmail.com>
2021-08-03 05:39:47 +0530

    Removed Templates code and updated Dependencies

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 const express = require('express');
-const path = require('path');
-const hbs = require('hbs');
 const app = express()
 const attendance = require('./utils/attendance')
-// Define path for Express Config
-const publicDirPath = path.join(__dirname, "public")
-const viewsPath = path.join(__dirname, "templates/views")
-const partialsPath = path.join(__dirname, "templates/partials")
+
 // port configuarion
 const port = process.env.PORT || 3030
 
-// Setting up the template engine
-app.set('view engine', 'hbs')
-// Setting up the path for template engine
-app.set('views', viewsPath)
-
-hbs.registerPartials(partialsPath);
-// To serve static files == Mostly not needed
-app.use(express.static(publicDirPath))
-app.get('', (req, res) => {
-    res.render('index')
+app.get('/', function (req, res) {
+    res.send(`Hello World from host ERP API!`)
 })
 
-app.get('/', function (req, res) {
-    const clientIP = req.headers['x-forwarded-for'];
-    res.send(`Hello World from host ${os.hostname()} ${clientIP}!`)
+app.get('/attendance', (req, res) => {
+    if (!req.query.rollNumber || !req.query.password) {
+        return res.send({
+            error: "Login and Password Must be provided"
+        })
+    }
+    else {
+        let rollNumber = req.query.rollNumber
+        let password = req.query.password
+        attendance(rollNumber, password).then((val) => {
+            return res.send(val)
+        }).catch((err) => {
+            res.send({
+                error: "Kindly Check your Roll Number And Password and Check again"
+            })
+        })
+    }
 })
 
 app.listen(port, () => {
